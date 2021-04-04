ef8f0915e1c6cc7d5c67fdc17a74182f7274f8d1
Misaka <misakachan2545@gmail.com>
2021-04-04 17:22:23 +0300

    Рефакторинг

---
+++ src/uwu/misaka/Ichi.java
@@ -0,0 +1,22 @@
+package uwu.misaka;
+
+import arc.util.*;
+import mindustry.mod.*;
+
+public class Ichi extends Plugin{
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void registerServerCommands(CommandHandler handler){
+
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler){
+
+    }
+}
